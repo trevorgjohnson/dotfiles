e0c15022ada0ab43840b8a0ac89f50b6216bc35d
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2023-09-11 12:02:21 -0500

    Remove 'check_backspace' func in 'cmp.lua'

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -4,11 +4,6 @@ local luasnip = require 'luasnip'
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_backspace = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-end
-
 --   פּ ﯟ   some other good icons
 local kind_icons = {
   Text = "󱄽",
