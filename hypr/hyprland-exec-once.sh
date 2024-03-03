87fdaf23ba3a0dcc2c15794b29b8a936db9930b0
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2024-03-03 11:11:06 +0000

    fix: adjust hypr portals to optimze app startup

--- hypr/hyprland-exec-once.sh
+++ hypr/hyprland-exec-once.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
+# Updates the list of environment variables used by the dbus-daemon
+echo "Updating the list of environment variables..."
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+
 # Kill any existing portals
 echo "Killing existing portals..."
-sleep 1
+sleep 4
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 
 # Set up portals
 echo "Setting up portals..."
-/usr/lib/xdg-desktop-portal-hyprland & 
-sleep 2 
-/usr/lib/xdg-desktop-portal &
+sleep 4
+systemctl --user start xdg-desktop-portal-hyprland
+systemctl --user start xdg-desktop-portal
 
 # Start Waybar (status bar) and Hyprpaper (wallpaper manager)
 echo "Starting Waybar and Hyprpaper..."
 /usr/bin/waybar &
 /usr/bin/hyprpaper &
-
-# Updates the list of environment variables used by the dbus-daemon
-echo "Updating the list of environment variables..."
-/usr/bin/dbus-update-activation-environment --systemd --all &
 
 echo "Hyprland was set up successfully!"
