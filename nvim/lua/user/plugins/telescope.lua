02e1ba3dc2e9855e49edf931980cc40bbfd3c933
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2024-05-09 08:32:02 -0500

    chore: add telescope <cr> select multiple support

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -1,3 +1,19 @@
+-- Allows <cr> in telescope to select one under the cursor or multiple using <tab> (similar to fzf)
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.5',
@@ -64,7 +80,7 @@ return {
             ["<Down>"] = actions.move_selection_next,
             ["<Up>"] = actions.move_selection_previous,
 
-            ["<CR>"] = actions.select_default,
+            ["<CR>"] = select_one_or_multi,
 
             ["<C-u>"] = actions.preview_scrolling_up,
             ["<C-d>"] = actions.preview_scrolling_down,
