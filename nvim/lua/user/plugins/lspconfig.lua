9a79e4a5299c39fc376ee20cb9f776689679e0d3
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2024-05-19 20:00:45 -0500

    Merge branch feat/nvim-v0.10 into main branch

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -1,156 +1,135 @@
+-- common inlay hints used for typescript and javascript
+local inlayHints = {
+  includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+  includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+  includeInlayVariableTypeHints = true,
+  includeInlayFunctionParameterTypeHints = true,
+  includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+  includeInlayPropertyDeclarationTypeHints = true,
+  includeInlayFunctionLikeReturnTypeHints = true,
+  includeInlayEnumMemberValueHints = true,
+}
+
 local servers = {
-  tsserver = {},
+  tsserver = {
+    settings = {
+      typescript = { inlayHints = inlayHints },
+      javascript = { inlayHints = inlayHints },
+    },
+  },
   tailwindcss = {},
   rust_analyzer = {
-    rust_analyzer = { settings = { checkOnSave = { command = "clippy" } } }
+    cargo = {
+      allFeatures = true,
+      loadOutDirsFromCheck = true,
+      runBuildScripts = true,
+    },
+    checkOnSave = {
+      allFeatures = true,
+      command = "clippy",
+      extraArgs = { "--no-deps" },
+    },
+    procMacro = {
+      enable = true,
+      ignored = {
+        ["async-trait"] = { "async_trait" },
+        ["napi-derive"] = { "napi" },
+        ["async-recursion"] = { "async_recursion" },
+      },
+    },
   },
   solidity_ls_nomicfoundation = {},
   lua_ls = {
     Lua = {
+      settings = { Lua = { completion = { callSnippet = 'Replace' } } },
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
     },
   }
 }
 
-local lsp_handler = {
-  setup = function()
-    local signs = { {
-      name = "DiagnosticSignError",
-      text = ""
-    }, {
-      name = "DiagnosticSignWarn",
-      text = ""
-    }, {
-      name = "DiagnosticSignHint",
-      text = ""
-    }, {
-      name = "DiagnosticSignInfo",
-      text = ""
-    } }
-
-    for _, sign in ipairs(signs) do
-      vim.fn.sign_define(sign.name, {
-        texthl = sign.name,
-        text = sign.text,
-        numhl = ""
-      })
-    end
-
-    vim.diagnostic.config({
-      -- disable virtual text
-      virtual_text = false,
-      -- show signs
-      signs = {
-        active = signs
-      },
-      update_in_insert = true,
-      underline = true,
-      severity_sort = true,
-      float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = ""
-      }
-    })
-
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-      border = "rounded"
-    })
-
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-      border = "rounded"
-    })
-  end,
-  on_attach = function(_, bufnr)
-    ---Sets `keys` to `func` and include description `desc` 
-    ---@param keys string key(s) used to call `func`
-    ---@param func function function called when `keys` are pressed
-    ---@param desc string the description of the key binding
-    local nmap = function(keys, func, desc)
-      if desc then
-        desc = 'LSP: ' .. desc
-      end
-
-      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-    end
-
-    nmap('K', vim.lsp.buf.hover, "[LSP]: Display documentation of a word")
-    nmap('<space>sk>', vim.lsp.buf.signature_help, "[LSP]: Displays [s]ignature help")
-
-    nmap('gd', vim.lsp.buf.definition, "[LSP]: [g]o to the [d]efinition of a word")
-    nmap('gD', vim.lsp.buf.declaration, "[LSP]: [g]o to the [D]eclaration of a word")
-    nmap('gi', vim.lsp.buf.implementation, "[LSP]: [g]o to the [i]mplementation of a word")
-
-    nmap('<space>fm', vim.lsp.buf.format, "[LSP]: [f]or[m]at file")
-    nmap('<space>td', vim.lsp.buf.type_definition, "[LSP]: Display [t]ype [d]efinition of a word")
-    nmap('<space>rn', vim.lsp.buf.rename, "[LSP]: [r]e[n]ame all references of a word")
-    nmap('<space>ca', vim.lsp.buf.code_action, "[LSP]: Display and attempt available [c]ode [a]ctions on a word")
-  end,
-}
+-- describes all of the LSP capabilities of Neovim and any defaults described by 'cmp_nvim_lsp'
+---@type table|lsp.ClientCapabilities|nil
+local capabilities;
 
 return {
   'neovim/nvim-lspconfig',
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    'folke/neodev.nvim',
-    "hrsh7th/cmp-nvim-lsp",
-    { "mrcjkb/rustaceanvim",                 version = "^4",                                        lazy = false, ft = "rust,toml", },
-    { 'j-hui/fidget.nvim',                   tag = 'legacy',                                        opts = {} },
-    { "antosha417/nvim-lsp-file-operations", config = true },
-    { "williamboman/mason.nvim",             dependencies = { "williamboman/mason-lspconfig.nvim" } },
+    -- LSP status updates
+    { 'j-hui/fidget.nvim',       opts = { notification = { window = { winblend = 0 } } } },
+
+    -- LSP installer
+    { "williamboman/mason.nvim", opts = {} },
+
+    { -- Links LSP installer to lspconfig
+      "williamboman/mason-lspconfig.nvim",
+      dependencies = { "hrsh7th/cmp-nvim-lsp" },
+      ensure_installed = vim.tbl_keys(servers or {}),
+      automatic_installation = true,
+      opts = {
+        handlers = {
+          function(server_name)
+            -- grabs capabilities from local cache if not already set
+            if not capabilities then
+              capabilities = vim.tbl_deep_extend('force', vim.lsp.protocol.make_client_capabilities(),
+                require('cmp_nvim_lsp').default_capabilities())
+            end
+
+            local server = servers[server_name] or {}
+            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            require('lspconfig')[server_name].setup(server)
+          end,
+          ['rust_analyzer'] = function() end, -- skip 'rust_analyzer' in favor of 'rustaceanvim'
+        }
+      }
+    },
+
+    -- Rust specific LSP tooling
+    {
+      "mrcjkb/rustaceanvim",
+      version = "^4",
+      lazy = false,
+      ft = "rust,toml",
+      opts = { server = { default_settings = { ['rust_analyzer'] = servers['rust_analyzer'] }, }, },
+      config = function(_, opts)
+        vim.g.rustaceanvim = vim.tbl_deep_extend("force", {
+          cmd = function()
+            local mason_registry = require('mason-registry')
+            local ra_binary = mason_registry.is_installed('rust-analyzer')
+                and mason_registry.get_package('rust-analyzer'):get_install_path() .. "/rust-analyzer"
+                or "rust-analyzer"
+            return { ra_binary }
+          end
+        }, opts or {})
+      end,
+    },
+
+    -- Lua specific LSP tooling
+    { 'folke/neodev.nvim', opts = {}, ft = "lua", },
   },
   config = function()
-    require('neodev').setup()                           -- Setup neovim lua configuration
-    require('fidget').setup({ window = { blend = 0 } }) -- Turn on lsp status information
-
-    lsp_handler.setup();                                -- Set up handlers for LSP
-
-    require('mason').setup()                            -- Set up LSP installer
-
-    local mason_lspconfig = require("mason-lspconfig")
-    mason_lspconfig.setup { ensure_installed = vim.tbl_keys(servers), }
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        if server_name == "rust_analyzer" then
-          ---@type RustaceanOpts
-          vim.g.rustaceanvim = {
-            server = {
-              on_attach = function(client, bufnr)
-                -- load default 'on_attach' capabilities
-                lsp_handler.on_attach(client, bufnr)
-
-                -- Use code actions through 'Rustaceanvim'
-                vim.keymap.set('n', '<space>ca', function() vim.cmd.RustLsp('codeAction') end,
-                  { silent = true, buffer = bufnr, desc = "[🦀-LSP]: Display and attempt available [c]ode [a]ctions on a word" })
-
-                -- automatically refresh codelens when entering/writing buffer
-                vim.api.nvim_create_autocmd({ "BufEnter", "BufWrite" },
-                  { callback = function() vim.lsp.codelens.refresh() end })
-              end,
-              default_settings = { servers.rust_analyzer },
-              cmd = function()
-                local mason_registry = require('mason-registry')
-                local ra_binary = mason_registry.is_installed('rust-analyzer')
-                    -- This may need to be tweaked, depending on the operating system.
-                    and mason_registry.get_package('rust-analyzer'):get_install_path() .. "/rust-analyzer"
-                    or "rust-analyzer"
-                return { ra_binary } -- You can add args to the list, such as '--log-file'
-              end
-            },
-          }
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('lsp-attach-group', { clear = true }),
+      callback = function(event)
+        local map = function(keys, func, desc)
+          vim.keymap.set('n', keys, func,
+            { buffer = event.buf, desc = '[LSP]: ' .. desc })
         end
 
-        require("lspconfig")[server_name].setup {
-          on_attach = lsp_handler.on_attach,
-          capabilities = lsp_handler.capabilities,
-          flags = lsp_handler.lsp_flags,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
-        }
+        map('gd', vim.lsp.buf.definition, "[g]o to the [d]efinition of a word")
+        map('<leader>fm', vim.lsp.buf.format, "[f]or[m]at file")
+        map('<leader>td', vim.lsp.buf.type_definition, "Display [t]ype [d]efinition of a word")
+        map('<leader>rn', vim.lsp.buf.rename, "[r]e[n]ame all references of a word")
+        map('<leader>ca', vim.lsp.buf.code_action, "Display and attempt available [c]ode [a]ctions on a word")
+
+        -- enables inlay hints in your code if the language server supports them
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+          map('<leader>th', function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(_))
+          end, '[t]oggle inlay [h]ints')
+        end
       end,
     })
   end
